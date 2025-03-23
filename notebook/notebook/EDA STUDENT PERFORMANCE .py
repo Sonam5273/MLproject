6b5c6679da797acccee3eeb001e56087678a131b
Sonam Kumari <sonamkumari14071999@gmail.com>
2025-03-23 11:06:54 +0530

    Data Ingestion

---
+++ notebook/notebook/EDA STUDENT PERFORMANCE .py
@@ -0,0 +1,9 @@
+#### Life cycle of Machine learning Project
+
+- Understanding the Problem Statement
+- Data Collection
+- Data Checks to perform
+- Exploratory data analysis
+- Data Pre-Processing
+- Model Training
+- Choose best model
